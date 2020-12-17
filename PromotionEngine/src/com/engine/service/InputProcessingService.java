f511cb5d6ba4e04252acabc73898762351b7c03f
ApurvJaiswal <apurvjaiz@gmail.com>
2020-12-17 16:10:40 +0530

    Added Service Class to pre-process the input SKU

---
+++ PromotionEngine/src/com/engine/service/InputProcessingService.java
@@ -0,0 +1,35 @@
+package com.engine.service;
+
+import java.util.HashMap;
+
+public class InputProcessingService 
+{
+
+	public char[] splitInput(String input) 
+	{
+		String splitRegex="\\s+";
+		String SKU[]=input.split(splitRegex);
+		char sku_id[]=new char[SKU.length];
+		int count=0;
+		for(String str:SKU)
+		{
+			sku_id[count++]=str.charAt(0);
+		}
+		return sku_id;
+	}
+
+	public HashMap<Character, Integer> getUnitCount(char[] sku_id) 
+	{
+		HashMap<Character, Integer> unit_count=new HashMap<Character, Integer>();
+		for(Character ch:sku_id)
+		{
+			if(!unit_count.containsKey(ch))
+				unit_count.put(ch, 0);
+			int count=unit_count.get(ch);
+			unit_count.put(ch, count+1);			
+		}
+		return unit_count;
+	}
+	
+
+}
